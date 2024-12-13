f32baa81437d2fc871d5d6d5243f43c7db7fbb9b
unknown <fabioflauzino@hotmail.com>
2024-12-13 01:27:19 -0300

    Funcionando o elevador através do controle

--- js/rua.js
+++ js/rua.js
@@ -1,18 +1,15 @@
-(function () {
+(function() {
   function criarFaixas() {
-    const elementosComFaixas = document.querySelectorAll('[faixas]')
-
-    elementosComFaixas.forEach(el => {
-      const qtde = Number(el.getAttribute('faixas'))
-      for (let i = 0; i < qtde; i++) {
-        const faixa = document.createElement('div')
-        faixa.classList.add('faixa')
-
-        el.appendChild(faixa)
-      }
-    })
+      const elementosComFaixas = document.querySelectorAll('[faixas]')
+      elementosComFaixas.forEach(el => {
+          const qtde = +el.getAttribute('faixas')
+          for (let i = 0; i < qtde; i++) {
+              const faixa = document.createElement('div')
+              faixa.classList.add('faixa')
+              el.appendChild(faixa)
+          }
+      })
   }
-
+  
   criarFaixas()
 })()
-
